325a419191c69ebc596327046e43e784e28b59a4
code13 <code__13@outlook.com>
2021-06-23 16:47:04 +0800

    :sparkles: vavr
    Tuples/Functions/Values

---
+++ prettier-libs/vavr/src/main/java/io/github/code13/javastack/libs/vavr/values/TryTest.java
@@ -0,0 +1,59 @@
+/*
+ *     Copyright 2021-present the original author or authors.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package io.github.code13.javastack.libs.vavr.values;
+
+import io.vavr.control.Try;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.vavr.API.*;
+import static io.vavr.Predicates.instanceOf;
+
+/**
+ * vavr Try
+ * <p>
+ * Try is a monadic container type which represents a computation that may either result in an exception, or return a successfully computed value.
+ * It’s similar to, but semantically different from Either.
+ * Instances of Try, are either an instance of Success or Failure.
+ *
+ * @author <a href="https://github.com/Code-13/">code13</a>
+ * @date 2021/6/23 16:05
+ */
+@DisplayName("Try")
+class TryTest {
+
+  @Test
+  @DisplayName("Example for Try")
+  void test1() {
+
+    Integer result = Try.of(this::bunchOfWork)
+      .recover(x -> Match(x).of(
+        Case($(instanceOf(IllegalArgumentException.class)), t -> 2),
+        Case($(instanceOf(NullPointerException.class)), t -> 3)
+      ))
+      .getOrElse(10);
+
+    System.out.println(result);
+  }
+
+  private int bunchOfWork() {
+    return 1;
+    //throw new IllegalArgumentException();
+    //throw new NullPointerException();
+  }
+
+}
