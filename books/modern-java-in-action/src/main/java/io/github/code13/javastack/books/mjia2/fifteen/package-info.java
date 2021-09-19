10f209f4c4d70c568a71466f7ca302380f7fb6ea
code13 <code__13@outlook.com>
2021-09-19 14:19:23 +0800

    :sparkles: books
    modern-java-in-action

---
+++ books/modern-java-in-action/src/main/java/io/github/code13/javastack/books/mjia2/fifteen/package-info.java
@@ -0,0 +1,30 @@
+/*
+ *     Copyright 2021-present the original author or authors.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+/**
+ * 15-Concepts-behind-CompletableFuture-and-reactive-programming.
+ *
+ * <pre>
+ * - 异步 API
+ * - 线框与管道
+ * - CompletableFuture 结合器动态地连接线框
+ * - 响应式编程与响应式系统
+ * </pre>
+ *
+ * @author <a href="https://github.com/Code-13/">code13</a>
+ * @date 2021/9/19 14:14
+ */
+package io.github.code13.javastack.books.mjia2.fifteen;
