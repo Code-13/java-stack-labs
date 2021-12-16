037bcf325991108af147b36e8e838033fa51278e
code13 <code__13@outlook.com>
2021-12-16 11:47:20 +0800

    :sparkles: java-concurrency-in-practice
    applying thread pool

---
+++ books/java-concurrency-in-practice/src/main/java/io/github/code13/javastack/books/jcip/ch8_applying_thread_pool/PuzzleSolver.java
@@ -0,0 +1,57 @@
+/*
+ *     Copyright 2021-present the original author or authors.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package io.github.code13.javastack.books.jcip.ch8_applying_thread_pool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * PuzzleSolver.
+ *
+ * @author <a href="https://github.com/Code-13/">code13</a>
+ * @date 12/16/2021 10:52 AM
+ */
+public class PuzzleSolver<P, M> extends ConcurrentPuzzleSolver<P, M> {
+
+  PuzzleSolver(Puzzle<P, M> puzzle) {
+    super(puzzle);
+  }
+
+  private final AtomicInteger taskCount = new AtomicInteger(0);
+
+  @Override
+  protected Runnable newTask(P p, M m, PuzzleNode<P, M> n) {
+    return new CountingSolverTask(p, m, n);
+  }
+
+  class CountingSolverTask extends SolverTask {
+    CountingSolverTask(P pos, M move, PuzzleNode<P, M> prev) {
+      super(pos, move, prev);
+      taskCount.incrementAndGet();
+    }
+
+    @Override
+    public void run() {
+      try {
+        super.run();
+      } finally {
+        if (taskCount.decrementAndGet() == 0) {
+          solution.setValue(null);
+        }
+      }
+    }
+  }
+}
---
+++ books/java-concurrency-in-practice/src/main/java/io/github/code13/javastack/books/jcip/ch8_applying_thread_pool/ValueLatch.java
@@ -0,0 +1,54 @@
+/*
+ *     Copyright 2021-present the original author or authors.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package io.github.code13.javastack.books.jcip.ch8_applying_thread_pool;
+
+import io.github.code13.javastack.books.jcip.GuardedBy;
+import io.github.code13.javastack.books.jcip.ThreadSafe;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * ValueLatch.
+ *
+ * @author <a href="https://github.com/Code-13/">code13</a>
+ * @date 12/16/2021 10:50 AM
+ */
+@ThreadSafe
+public class ValueLatch<T> {
+
+  @GuardedBy("this")
+  private T value = null;
+
+  private final CountDownLatch done = new CountDownLatch(1);
+
+  public boolean isSet() {
+    return (done.getCount() == 0);
+  }
+
+  public synchronized void setValue(T newValue) {
+    if (!isSet()) {
+      value = newValue;
+      done.countDown();
+    }
+  }
+
+  public T getValue() throws InterruptedException {
+    done.await();
+    synchronized (this) {
+      return value;
+    }
+  }
+}
