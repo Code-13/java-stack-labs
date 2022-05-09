0f3ed4de9ff40e681a042fc25885dc4110a484be
code13 <code__13@outlook.com>
2022-05-09 14:31:31 +0800

    :sparkles: objenesis

--- prettier-libs/objenesis/src/main/java/io/github/code13/javastack/libs/objenesis/ObjenesisRunner.java
+++ prettier-libs/objenesis/src/main/java/io/github/code13/javastack/libs/objenesis/ObjenesisRunner.java
@@ -1,17 +1,17 @@
 /*
- *     Copyright 2021-present the original author or authors.
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ * Copyright 2022-present the original author or authors.
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package io.github.code13.javastack.libs.objenesis;
@@ -49,9 +49,31 @@ class ObjenesisRunner {
     System.out.println(user);
   }
 
+  @Test
+  @DisplayName("testNotNeedCons")
+  void testNotNeedCons() {
+    HelloObjenesis instance = objenesis.newInstance(HelloObjenesis.class);
+    System.out.println(instance);
+    System.out.println(instance.hello);
+    // io.github.code13.javastack.libs.objenesis.ObjenesisRunner$HelloObjenesis@33cb5951
+    // null
+  }
+
   @Data
   static class User {
     private String userName;
     private Integer age;
+  }
+
+  static class HelloObjenesis {
+    private String hello = "Objenesis!";
+
+    public HelloObjenesis(String hello) {
+      this.hello = hello;
+    }
+
+    public void hello() {
+      System.out.println("你好！ " + hello);
+    }
   }
 }
