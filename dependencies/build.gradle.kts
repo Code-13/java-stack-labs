9dc0f9754627e5b6f19f3983547693089fcafd27
code13 <code__13@outlook.com>
2024-05-17 21:42:55 +0800

    :arrow_up: project

--- dependencies/build.gradle.kts
+++ dependencies/build.gradle.kts
@@ -7,19 +7,19 @@ javaPlatform {
 }
 
 dependencies {
-    api(platform("org.springframework.boot:spring-boot-dependencies:3.2.4"))
-    api(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.0"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:3.2.5"))
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.1"))
     constraints {
-        api("com.baomidou:mybatis-plus-generator:3.5.5")
-        api("com.baomidou:mybatis-plus-boot-starter:3.5.5")
-        api("com.baomidou:mybatis-plus-core:3.5.5")
-        api("com.baomidou:mybatis-plus-extension:3.5.5")
-        api("com.baomidou:mybatis-plus-extension:3.5.5")
-        api("com.baomidou:mybatis-plus-annotation:3.5.5")
+        api("com.baomidou:mybatis-plus-generator:3.5.6")
+        api("com.baomidou:mybatis-plus-boot-starter:3.5.6")
+        api("com.baomidou:mybatis-plus-core:3.5.6")
+        api("com.baomidou:mybatis-plus-extension:3.5.6")
+        api("com.baomidou:mybatis-plus-extension:3.5.6")
+        api("com.baomidou:mybatis-plus-annotation:3.5.6")
 
-        api("com.easy-query:sql-mysql:1.10.44")
-        api("com.easy-query:sql-api-proxy:1.10.44")
-        api("com.easy-query:sql-processor:1.10.44")
+        api("com.easy-query:sql-mysql:2.0.13")
+        api("com.easy-query:sql-api-proxy:2.0.13")
+        api("com.easy-query:sql-processor:2.0.13")
 
         api("org.mapstruct:mapstruct:1.5.5.Final")
         api("oorg.mapstruct:mapstruct-processor:1.5.5.Final")
@@ -32,7 +32,7 @@ dependencies {
         api("io.vavr:vavr-test:0.10.3")
         api("io.vavr:vavr-beanvalidation2:0.10.0")
 
-        api("commons-io:commons-io:2.15.1")
+        api("commons-io:commons-io:2.16.1")
         api("org.apache.commons:commons-collections4:4.4")
         api("commons-beanutils:commons-beanutils:1.9.4")
         api("commons-fileupload:commons-fileupload:1.5")
@@ -46,20 +46,20 @@ dependencies {
 
         api("org.jsoup:jsoup:1.17.2")
         api("org.reflections:reflections:0.10.2")
-        api("com.googlecode.libphonenumber:libphonenumber:8.13.33")
+        api("com.googlecode.libphonenumber:libphonenumber:8.13.37")
         api("org.locationtech.spatial4j:spatial4j:0.8")
         api("com.google.zxing:core:3.5.3")
 
-        api("net.datafaker:datafaker:2.1.0")
-        api("org.objenesis:objenesis:3.3")
+        api("net.datafaker:datafaker:2.2.2")
+        api("org.objenesis:objenesis:3.4")
         api("io.protostuff:protostuff-core:1.8.0")
         api("io.protostuff:protostuff-runtime:1.8.0")
         api("com.alibaba:easyexcel:3.3.4")
 
-        api("org.redisson:redisson:3.27.2")
-        api("org.redisson:redisson-spring-boot-starter:3.27.2")
-        api("org.redisson:redisson-mybatis:3.27.2")
-        api("org.redisson:redisson-spring-data-32:3.27.2")
+        api("org.redisson:redisson:3.30.0")
+        api("org.redisson:redisson-spring-boot-starter:3.30.0")
+        api("org.redisson:redisson-mybatis:3.30.0")
+        api("org.redisson:redisson-spring-data-32:3.30.0")
 
         api("org.openjdk.jmh:jmh-core:1.37")
         api("org.openjdk.jmh:jmh-generator-annprocess:1.37")
--- spring/spring-boot/caffeine-redis-multi-level-cache/build.gradle.kts
+++ spring/spring-boot/caffeine-redis-multi-level-cache/build.gradle.kts
@@ -9,5 +9,5 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.2.4")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:3.2.5")
 }
